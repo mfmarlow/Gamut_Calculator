4188cdef4bd0c97445849628968f3f1a523981a7
Matthew Marlow <sk8rbohy@gmail.com>
2024-06-20 17:16:54 -0700

    Add input validation and changed CSV file format support

--- src/main.py
+++ src/main.py
@@ -60,13 +60,13 @@ class Gamut_win(QtWidgets.QMainWindow):
 
     def f_table_s(self):
         if self.sender().isChecked():
-            self.ui.le_sample_name.setEnabled(True)
+            # self.ui.le_sample_name.setEnabled(True)
             self.ui.tW_sample.setEnabled(True)
             self.ui.groupBox_import_sample.setEnabled(False)
 
     def f_file_s(self):
         if self.sender().isChecked():
-            self.ui.le_sample_name.setEnabled(False)
+            # self.ui.le_sample_name.setEnabled(False)
             self.ui.tW_sample.setEnabled(False)
             self.ui.groupBox_import_sample.setEnabled(True)
 
@@ -87,53 +87,12 @@ class Gamut_win(QtWidgets.QMainWindow):
     def calculate(self):
 
         samples = user_input.get_colorspace_input(self)
-        print(samples)
 
         for sample in samples:
-            sample_name = sample[0]
-            sample_r = np.array([float(sample[1]), float(sample[2])])
-            sample_g = np.array([float(sample[3]), float(sample[4])])
-            sample_b = np.array([float(sample[5]), float(sample[6])])
-            sample = np.array([sample_r, sample_g, sample_b])
-            if not self.table_cell_error:
-
-                # Create the background image
-                figure = plt.figure(figsize=(10, 11))
-                background_axes = figure.add_axes([0.03, -0.025, 0.962, 0.962])
-                image = mpimg.imread("assets/1080px-CIE_1976_UCS.png")
-                background_axes.imshow(image)
-                background_axes.axis('off')
-                # colour.plotting.plot_chromaticity_diagram_CIE1976UCS(axes=axes, transparent_background=False)
-
-                # Add axes for plotting the user input points
-                axes = figure.add_axes([0.0062, -0.0028, 1.01, 0.92], frame_on=False)
-                axes.axis("off")
-
-                # Set title and subtitle
-                axes.set_title('Chromaticity Diagram', y=1.021, fontsize=42, fontweight='bold')
-                plt.text(0.5, 0.982, sample_name, ha='center', va='center', transform=axes.transAxes, fontsize=30)
-
-                # Adjust subplot parameters to create more space around the plot
-                plt.subplots_adjust(left=0.07, right=0.9, top=0.9, bottom=0.07)
-
-                # Set the x and y axis limits
-                axes.set_xlim(-0.05, 0.65)
-                axes.set_ylim(-0.05, 0.65)
-
-                # Set a higher z-order for the chromaticity diagram elements
-                for artist in axes.get_children():
-                    artist.set_zorder(2)
-                    artist.set_clip_on(False)
-                axes.get_children()[1].set_clip_on(True)
-
-                # Replace 0s with really small numbers instead to avoid division by 0
-                EPSILON: float = colour.hints.cast(float, np.finfo(np.float_).eps)
-
-                points = np.where(
-                    sample == 0,
-                    EPSILON,
-                    sample,
-                )
+            input_is_valid = self.validate_input(sample)
+            if input_is_valid:
+                points, sample_name = self.extract_points(sample)
+                axes = self.setup_plot(sample_name)
 
                 # xys = colour.models.Luv_uv_to_xy(points)
                 # xyzs = colour.models.xy_to_XYZ(xys)
@@ -141,44 +100,112 @@ class Gamut_win(QtWidgets.QMainWindow):
                 # rgbs = np.clip(raw_rgbs, 0.0, 1.0) #TODO is clipping the right strategy?
 
                 points_plus_first = np.vstack([points, points[0]])
-                axes.plot(points_plus_first[..., 0], points_plus_first[...,1], color='black', linewidth=4, zorder=3)
-
-                axes.scatter(points[..., 0], points[..., 1], c='black',edgecolor='black', s=250, linewidths=2.5, zorder=4)
-
-                # def generate_color_dots(axes: Axes):
-                #     xs = []
-                #     ys = []
-                #     num_points = 40
-                #     for i in range(num_points):
-                #         for j in range(num_points):
-                #             xs = np.append(xs, i/(num_points-1))
-                #             ys = np.append(ys, j/(num_points-1))
-                #     more_points = np.vstack([xs, ys])
-                #     more_points = np.transpose(more_points)
-
-                #     xys = colour.models.Luv_uv_to_xy(more_points)
-                #     xyzss = colour.models.xy_to_XYZ(xys)
-                #     rgbss = np.clip(colour.models.XYZ_to_sRGB(xyzss), 0.0, 1.0)
-
-                #     axes.scatter(more_points[...,0], more_points[...,1], c= rgbss, edgecolor='black', s=50, linewidths=2.5 , zorder=3)
-
-                # generate_color_dots(axes= axes)
-
-                def triangle_area(vertices):
-                    # Unpack the vertices
-                    x1, y1 = vertices[0]
-                    x2, y2 = vertices[1]
-                    x3, y3 = vertices[2]
-
-                    # Calculate the area using the determinant formula
-                    area = 0.5 * abs(x1*(y2 - y3) + x2 *(y3 - y1) + x3*(y1 - y2))
-                    return area
-
-                axes.legend([f'Area: {triangle_area(points):0.4f}'], bbox_to_anchor=(0.9, 0.25), handlelength=0, fontsize=20, framealpha=1)
+                self.draw_triangle(axes, points_plus_first)
+                self.draw_points(points, axes)
+                self.draw_legend(points, axes)
+                # self.draw_color_dots(axes= axes)
                 plt.show()
                 self.save_to_file(sample_name)
             else:
-                print("table cell error")
+                print("Invalid input. Please verify input is correct")
+                print(sample)
+
+    def validate_input(self, sample):
+        if sample.size != 7:
+            return False
+        # Check table values if empty and between 0 and 0.9
+        for value in sample[1:]:
+            try:
+                if float(value) <= 0 or float(value) >= 0.9:
+                    return False
+            except :
+                return False
+        return True
+
+    def extract_points(self, sample):
+        sample_name = sample[0]
+        sample_r = np.array([float(sample[1]), float(sample[2])])
+        sample_g = np.array([float(sample[3]), float(sample[4])])
+        sample_b = np.array([float(sample[5]), float(sample[6])])
+        sample = np.array([sample_r, sample_g, sample_b])
+    
+        # Replace 0s with really small numbers instead to avoid division by 0
+        EPSILON: float = colour.hints.cast(float, np.finfo(np.float_).eps)
+
+        points = np.where(
+            sample == 0,
+            EPSILON,
+            sample,
+        )
+
+        return points,sample_name
+
+    def setup_plot(self, sample_name):
+        # Create the background image
+        figure = plt.figure(figsize=(10, 11))
+        background_axes = figure.add_axes([0.03, -0.025, 0.962, 0.962])
+        image = mpimg.imread("assets/1080px-CIE_1976_UCS.png")
+        background_axes.imshow(image)
+        background_axes.axis('off')
+        # colour.plotting.plot_chromaticity_diagram_CIE1976UCS(axes=axes, transparent_background=False)
+
+        # Add axes for plotting the user input points
+        axes = figure.add_axes([0.0062, -0.0028, 1.01, 0.92], frame_on=False)
+        axes.axis("off")
+
+        # Set title and subtitle
+        axes.set_title('Chromaticity Diagram', y=1.021, fontsize=42, fontweight='bold')
+        plt.text(0.5, 0.982, sample_name, ha='center', va='center', transform=axes.transAxes, fontsize=30)
+
+        # Adjust subplot parameters to create more space around the plot
+        plt.subplots_adjust(left=0.07, right=0.9, top=0.9, bottom=0.07)
+
+        # Set the x and y axis limits
+        axes.set_xlim(-0.05, 0.65)
+        axes.set_ylim(-0.05, 0.65)
+
+        # Set a higher z-order for the chromaticity diagram elements
+        for artist in axes.get_children():
+            artist.set_zorder(2)
+            artist.set_clip_on(False)
+        axes.get_children()[1].set_clip_on(True)
+        return axes
+    
+    def triangle_area(self, vertices):
+        # Unpack the vertices
+        x1, y1 = vertices[0]
+        x2, y2 = vertices[1]
+        x3, y3 = vertices[2]
+
+        # Calculate the area using the determinant formula
+        area = 0.5 * abs(x1*(y2 - y3) + x2 *(y3 - y1) + x3*(y1 - y2))
+        return area
+
+    def draw_triangle(self, axes, points_plus_first):
+        axes.plot(points_plus_first[..., 0], points_plus_first[...,1], color='black', linewidth=4, zorder=3)
+
+    def draw_points(self, points, axes):
+        axes.scatter(points[..., 0], points[..., 1], c='black',edgecolor='black', s=250, linewidths=2.5, zorder=4)
+
+    def draw_legend(self, points, axes):
+        axes.legend([f'Area: {self.triangle_area(points):0.4f}'], bbox_to_anchor=(0.9, 0.25), handlelength=0, fontsize=20, framealpha=1)
+
+    def draw_color_dots(axes):
+        xs = []
+        ys = []
+        num_points = 40
+        for i in range(num_points):
+            for j in range(num_points):
+                xs = np.append(xs, i/(num_points-1))
+                ys = np.append(ys, j/(num_points-1))
+        more_points = np.vstack([xs, ys])
+        more_points = np.transpose(more_points)
+
+        xys = colour.models.Luv_uv_to_xy(more_points)
+        xyzss = colour.models.xy_to_XYZ(xys)
+        rgbss = np.clip(colour.models.XYZ_to_sRGB(xyzss), 0.0, 1.0)
+
+        axes.scatter(more_points[...,0], more_points[...,1], c= rgbss, edgecolor='black', s=50, linewidths=2.5 , zorder=3)
 
     def save_to_file(self, sample_name):
         self.save_to_file_path = self.ui.le_file_directory.text()
@@ -189,7 +216,6 @@ class Gamut_win(QtWidgets.QMainWindow):
                 print(f"unable to save file to path: {self.save_to_file_path}\\{sample_name}_chroma.png")
         else:
             print("Not saving image")
-
 
 app = QtWidgets.QApplication(sys.argv)
 win = Gamut_win()
--- src/user_input.py
+++ src/user_input.py
@@ -1,13 +1,6 @@
 import numpy as np
 import csv
-
-
-def get_sample_name(self):
-    if self.ui.le_sample_name.text().strip() != "":
-        WHITEPOINT_NAME_SAMPLE = self.ui.le_sample_name.text()
-    else:
-        WHITEPOINT_NAME_SAMPLE = "Sample"
-    return WHITEPOINT_NAME_SAMPLE
+from numpy._typing import NDArray
 
 
 def check_values_within_limits(self):
@@ -28,26 +21,36 @@ def get_colorspace_input(self):
     if self.ui.rB_file_s.isChecked():
         # Get sample name name from line edit form or set generic name if empty
         file_path = self.ui.le_browse_sample.text()
-        with open(file_path, "r") as file:
+        with open(file_path,"r", encoding='utf8', errors='replace') as file:
             csv_rows = csv.reader(file)
             data = list(csv_rows)
             array = np.array(data)
-            SAMPLES = np.delete(array, 0, 0)
-            return SAMPLES
+            samples_u_prime = array[11]
+            samples_v_prime = array[12]
+            SAMPLE = np.array(
+                [
+                    np.array(
+                        [
+                            get_sample_name(self),
+                            samples_u_prime[1], samples_v_prime[1],
+                            samples_u_prime[2], samples_v_prime[2],
+                            samples_u_prime[3], samples_v_prime[3]
+                        ]
+                    )
+                ]
+            )
+            return SAMPLE
 
     elif self.ui.rB_table_s.isChecked():
         # Check table values if empty and between 0 and 0.9
         check_values_within_limits(self)
-
-        # Get sample name name from line edit form or set generic name if empty
-        SAMPLE_NAME = get_sample_name(self)
 
         # Get primary RGB uv coordinates from table widget cells
         SAMPLE = np.array(
             [
                 np.array(
                     [
-                        SAMPLE_NAME,
+                        get_sample_name(self),
                         self.ui.tW_sample.item(0, 0).text(), self.ui.tW_sample.item(0, 1).text(),   #R
                         self.ui.tW_sample.item(1, 0).text(), self.ui.tW_sample.item(1, 1).text(),   #G
                         self.ui.tW_sample.item(2, 0).text(), self.ui.tW_sample.item(2, 1).text(),   #B
@@ -57,3 +60,10 @@ def get_colorspace_input(self):
         )
 
         return SAMPLE
+
+def get_sample_name(self):
+    if self.ui.le_sample_name.text().strip() != "":
+        WHITEPOINT_NAME_SAMPLE = self.ui.le_sample_name.text()
+    else:
+        WHITEPOINT_NAME_SAMPLE = "Placeholder"
+    return WHITEPOINT_NAME_SAMPLE
